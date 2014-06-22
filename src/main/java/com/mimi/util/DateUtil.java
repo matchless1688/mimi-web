0ef116bf0d6990cdb09049ac362a4839d3d6145a
matchless <jay.zl1688@163.com>
2014-06-22 10:48:30 +0800

    mimi new

---
+++ src/main/java/com/mimi/util/DateUtil.java
@@ -0,0 +1,13 @@
+package com.mimi.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	public static String format(Date date) {
+		return sdf.format(date);
+	}
+}
